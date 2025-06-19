dcac673315fd9f729b61e41ca9e69a3c3893994e
saran772 <saranpreetsinghsabharwal@gmail.com>
2025-06-19 18:15:55 +0530

    API CALLS get put post

--- src/app.js
+++ src/app.js
@@ -1,14 +1,25 @@
 const express=require("express")
 const app=express();
-app.use((req,res)=>{
-    res.send("hello server")
+
+app.get("/user",(req,res)=>{
+    res.send({firstname:"saranpreet",lastname:"singh"})
 })
-app.use("/",(req,res)=>{
-    res.send("hello")
+app.put("/user",(req,res)=>{
+    res.send("data put succefully")
 })
-app.use("/test",(req,res)=>{
-    res.send("hello")
+app.patch("/user",(req,res)=>{
+    res.send("data patch succefully")
 })
+app.delete("/user",(req,res)=>{
+    res.send("data delete succefully")
+})
+app.post("/user",(req,res)=>{
+    res.send("data send succefully")
+})
+
 app.listen(7777,()=>{
     console.log("data connected succesfully!")
 });
+
+//in app.use we've set up route which responds to all the http methods
+//but if we want limited access then us get put patch etc instead of app.use
