f56f5dc22d964d5b62a8b9476c7324d099eb56b1
saran772 <saranpreetsinghsabharwal@gmail.com>
2025-06-30 15:35:30 +0530

    Feed Api

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express=require("express")
 const { userauth } = require("../middleware/auth")
 const userRouter=express.Router()
 const ConnectionRequestModel=require("../models/ConnectionRequestModel")
+const User=require("../models/user")
 
 userRouter.get("/user/requests/received",userauth,async(req,res)=>{
     try{
@@ -40,6 +41,34 @@ userRouter.get("/user/connections",userauth,async(req,res)=>{
     }
      catch(err){
         req.statusCode(400).json({message:  "error detected"})
+    }
+})
+userRouter.get("/feed",userauth,async(req,res)=>{
+    try{
+        const loggedInUser=req.user;
+        const connectionreq=await ConnectionRequestModel.find({
+            $or:[
+                {fromuserId:loggedInUser._id},{touserid:loggedInUser._id}
+            ]
+        }).select("fromuserId touserid")
+
+        const hideusersfromfeed=new Set();
+        connectionreq.forEach((req)=>{
+            hideusersfromfeed.add(req.fromuserId.toString())
+            hideusersfromfeed.add(req.touserid.toString())
+        })
+
+        const users=await User.find({
+            $and:[
+                {_id:{$nin:Array.from(hideusersfromfeed)},},
+                {_id:{$ne:loggedInUser._id}}
+            ]
+        }).select("firstName lastName age gender skills")
+        res.send(users)
+console.log(users)
+    }
+    catch(err){
+        res.status(400).json({message:"error"})
     }
 })
 module.exports=userRouter
